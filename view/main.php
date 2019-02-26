a86d2ab49ee16865a2d16ddb2cf0fe49fcb9ad32
cvetelin98 <user-05@Trainings-Mac-mini-5.local>
2019-02-26 16:56:21 +0200

    main page styling

---
+++ view/main.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>nastop</title>
+    <link rel="stylesheet" href="../Style/style.css">
+</head>
+<body>
+<header id="mainHeader">
+    <h1 id="mainName">Nastop.bg</h1>
+    <div id="mainLinks">
+        <a href="login.html">Login</a>
+        |
+        <a href="register.html">Register</a>
+        <span id="addTravel">
+            + Wanna share a travel?
+        </span>
+    </div>
+</header>
+<img id="mainCover" src="https://static1.squarespace.com/static/55c1d8bce4b081fdca9dc5fd/t/573c76938259b5b384b45f7e/1463580310514/Individuals.jpg?format=1500w" width="80%" height="250px;">
+<main id="mainInMain">
+    <br>
+<p style="font-size: 30px;">&nbsp Selected Travels for sharing:</p>
+
+
+</main>
+<footer>
+
+</footer>
+
+</body>
+</html>
