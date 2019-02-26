828d161c3364377624bb7379207ff1ccf3843b48
yoannakadiyska <yoannna.kadiyska@gmail.com>
2019-02-26 17:36:25 +0200

    navigation bar & styling

---
+++ view/profile.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Edit</title>
+</head>
+<body>
+<ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="#news">My shared travels</a></li>
+    <li><a href="#contact">Profile edit</a></li>
+    <li><a href="#about">Add car</a></li>
+    <li><a href="#about">Change password</a></li>
+</ul>
+
+
+</body>
+</html>
